45dace1fa77dda26ba515dd2a7bc01778f18b745
Coda Bool <codabool@pm.me>
2023-07-31 17:14:35 -0400

    use instance for output

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,5 +1,8 @@
+# output "instance" {
+#   value = aws_spot_instance_request.main
+# }
 output "instance" {
-  value = aws_spot_instance_request.main
+  value = aws_instance.main
 }
 
 output "eip" {
