05181122e61910bc7dd791f3501652bc188dd7f4
Coda Bool <codabool@pm.me>
2023-10-23 02:31:54 -0400

    rm out

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,9 +1,9 @@
 # output "instance" {
 #   value = aws_spot_instance_request.main
 # }
-output "instance" {
-  value = aws_instance.main
-}
+# output "instance" {
+#   value = aws_instance.main
+# }
 
 # output "eip" {
 #   value = aws_eip.main.public_ip
