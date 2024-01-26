751feb2cce14c45e9f3bd40fa8d3b36f7240aa63
Coda Bool <codabool@pm.me>
2024-01-26 15:29:58 -0500

    switch to credential helper. Update supabase creds

--- modules/lambda/push.sh
+++ modules/lambda/push.sh
@@ -14,6 +14,17 @@ image_name_and_tag="$(echo "$repository_url" | cut -d/ -f2)"
 # subshell
 (cd "$path_to_dockerfile" && docker build -t "$image_name_and_tag" .)
 
-aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$repository_url"
+# I've switched to using aws ecr credential helper. This no longer uses docker login
+# https://github.com/awslabs/amazon-ecr-credential-helper
+
+if [ -f ~/.docker/config.json ]; then
+    # see if using ecr credential helper
+    if grep -q '"credsStore": "ecr-login"' ~/.docker/config.json; then
+        echo "no need to login when using ecr credential helper"
+    else
+        aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$repository_url"
+    fi
+fi
+
 docker tag "$image_name_and_tag" "$repository_url":"$tag"
 docker push "$repository_url":"$tag"
